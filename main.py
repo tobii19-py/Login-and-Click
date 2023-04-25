e85f45be7e773c80ca0247274ae90a6c9833b729
tobii19-py <129593840+tobii19-py@users.noreply.github.com>
2023-04-25 23:00:32 +0000

    Login and scrape value of just the temperature value without the string

--- main.py
+++ main.py
@@ -15,12 +15,27 @@ def get_driver():
   driver = webdriver.Chrome(options=options)
   driver.get("https://automated.pythonanywhere.com/login/")
   return driver
+
+def clean_text(text):
+  """Extract only the temperature from text"""
+  output = float(text.split(": ")[1])
+  return output
   
 def main():
   driver = get_driver()
+
+  # Find and fill in username and password
   driver.find_element(by="id", value="id_username").send_keys("automated")
   time.sleep(2)
   driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
   time.sleep(2)
+
+  # Click home button and wait 2 seconds
   driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-  print(driver.current_url)
+  time.sleep(2)
+
+  # Select temperature element
+  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+  return clean_text(element.text)
+
+print(main())
