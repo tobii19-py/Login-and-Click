61dbc46a4527b77c65b75585e37ef77d4ff0d98c
tobii19-py <129593840+tobii19-py@users.noreply.github.com>
2023-04-27 00:19:15 +0000

    Implemented the store temperature to text file function

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+from datetime import datetime
 
 def get_driver():
   # Set options to make browsing easier
@@ -20,22 +21,36 @@ def clean_text(text):
   """Extract only the temperature from text"""
   output = float(text.split(": ")[1])
   return output
+
+def store(temperature):
+  now = datetime.now().strftime("%Y-%m-%d-%H-%M-%S")
+  # assign current datetime to filename
+  filename = f"{now}.txt"
+  with open(filename, "a") as file:
+    file.write(str(temperature) + "\n")
+  return filename
   
 def main():
-  driver = get_driver()
+  while True:
+    driver = get_driver()
+  
+    # Find and fill in username and password
+    driver.find_element(by="id", value="id_username").send_keys("automated")
+    time.sleep(2)
+    driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
+    time.sleep(2)
+  
+    # Click home button and wait 2 seconds
+    driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
+    time.sleep(2)
+  
+    # Select temperature element
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
+    temperature = clean_text(element.text)
+    print(temperature)
 
-  # Find and fill in username and password
-  driver.find_element(by="id", value="id_username").send_keys("automated")
+    store(temperature)
+  
   time.sleep(2)
-  driver.find_element(by="id", value="id_password").send_keys("automatedautomated" + Keys.RETURN)
-  time.sleep(2)
 
-  # Click home button and wait 2 seconds
-  driver.find_element(by="xpath", value="/html/body/nav/div/a").click()
-  time.sleep(2)
-
-  # Select temperature element
-  element = driver.find_element(by="xpath", value="/html/body/div[1]/div/h1[2]")
-  return clean_text(element.text)
-
-print(main())
+main()
